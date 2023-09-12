48d589cc95059421d22d60924d5477cf90de39b4
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2023-09-12 13:36:14 +0530

    PostOrder Traversal

---
+++ 145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    void postOrder(TreeNode* root)
+    {
+        if(!root) return ;
+
+        postOrder(root->left);
+        postOrder(root->right);
+        ans.push_back(root->val);
+
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) 
+    {
+        postOrder(root);
+        return ans;
+    }
+};
